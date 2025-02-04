a733a125b936ae2b82dbcce972955fb086e89910
song7446 <songwon4529@naver.com>
2025-02-04 10:26:10 +0900

    시작씬 추가

--- TextRpg/Program.cs
+++ TextRpg/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "";
+
+            Console.WriteLine($"스파르타 던전에 오신걸 환영합니다!");
+            name = SettingName();
+
+            StartScene();
+        }
+
+        public static string SettingName()
+        {
+            Console.WriteLine("이름을 입력해주세요");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        public static string StartScene()
+        {
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+            Console.WriteLine("1. 상태 보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상점");
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            string input = Console.ReadLine();
+
+            return input;
         }
     }
 }
